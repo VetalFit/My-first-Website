6842b618de80a0e627dde65ce7127cfeaf17acb0
Vetal <v95664191@gmail.com>
2023-11-12 13:04:51 +0200

    Ready

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -42,7 +42,7 @@ const forms = (state) => {
 				for (let key in state) {
 					formData.append(key, state[key]);
 				}
-			}
+			};
 
 			postData('assets/server.php', formData)
 				.then(res => {
